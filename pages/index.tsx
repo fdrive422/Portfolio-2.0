97b1bbce2f930a7c2e43c079c3fc9b57321b3cad
fdrive422 <fafrago@gmail.com>
2022-09-23 22:02:06 -0700

    contact section, update scrollbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import About from "../components/About";
+import ContactMe from "../components/ContactMe";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
@@ -9,7 +13,7 @@ import WorkExperience from "../components/WorkExperience";
 
 const Home: NextPage = () => {
 	return (
-		<div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+		<div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
 			<Head>
 				<title>My Porfolio</title>
 			</Head>
@@ -23,19 +27,34 @@ const Home: NextPage = () => {
 			<section id="about" className="snap-center">
 				<About />
 			</section>
-			{/* Experiance */}
+
 			<section id="experience" className="snap-center">
 				<WorkExperience />
 			</section>
-			{/* Skills  */}
+
 			<section id="skills" className="snap-start">
 				<Skills />
 			</section>
-			{/* Projects */}
+
 			<section id="projects" className="snap-start">
 				<Projects />
 			</section>
-			{/* Contact Me */}
+
+			<section id="contact" className="snap-start">
+				<ContactMe />
+			</section>
+
+			<Link href="#hero">
+				<footer className="sticky bottom-5 w-full cursor-pointer">
+					<div className="flex items-center justify-center">
+						<img
+							className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+							src="https://pbs.twimg.com/profile_images/378800000166471388/dcaf75c494ce6bfe20603ebcc144eca7_400x400.jpeg"
+							alt=""
+						/>
+					</div>
+				</footer>
+			</Link>
 		</div>
 	);
 };
