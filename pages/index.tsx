994db580c925279613cc508b4d3a333f31d4df8e
fdrive422 <fafrago@gmail.com>
2022-09-26 21:40:34 -0700

    delpoyment fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
 	return (
 		<div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
 			<Head>
-				<title>My Porfolio</title>
+				<title>{pageInfo?.name} - Porfolio</title>
 			</Head>
 
 			<Header socials={socials} />
